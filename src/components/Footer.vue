ee80a738aa91ec0cf524378f7ac3ab930ae308d3
SQsuqi <643352633@qq.com>
2025-12-21 21:31:20 +0800

    chore(footer): update GitHub repository URL
    - Changed href from sqsuqi/myvite to suqi776/go-to-vue to reflect new repository location.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,7 +7,7 @@
     <a
       i-carbon-logo-github icon-btn
       rel="noreferrer"
-      href="https://github.com/sqsuqi/myvite"
+      href="https://github.com/suqi776/go-to-vue"
       target="_blank"
       title="GitHub"
     />
